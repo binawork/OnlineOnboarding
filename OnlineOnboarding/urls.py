18cf89f8547e0f02da86d4a67be9a4765cc4d66c
michalkandybowicz <eejdzent@gmail.com>
2020-05-08 13:30:56 +0200

    add create package

--- OnlineOnboarding/urls.py
+++ OnlineOnboarding/urls.py
@@ -28,8 +28,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-#Add Django site authentication urls (for login, logout, password management)
+# Add Django site authentication urls (for login, logout, password management)
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
