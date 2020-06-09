8363326d758f3a1d5270efe8279f6158f3e8efbd
michalkandybowicz <eejdzent@gmail.com>
2020-06-09 16:35:39 +0200

    - JS - D&D

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     url('signup', core_views.signup, name='signup'),
     path('manage/', views.manager_view, name='manage'),
-    path('package/<int:pk>/', views.pages_in_package, name='package'),
+    path('package/<int:pk>/', views.package_view, name='package'),
+    path('page/<int:pk>/', views.page_view, name='page'),
 ]
 
