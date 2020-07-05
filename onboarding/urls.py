e9e04c4a1c67a61176bc11d979fbdcb55ba72654
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-07-05 19:05:21 +0200

    Add bootstrap styled version of sign-in page

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('manage/', views.manager_view, name='manage'),
     path('package/<int:pk>/', views.package_view, name='package'),
     path('page/<int:pk>/', views.page_view, name='page'),
+
+    path('bootstrap/signin', views.bootstrap_signin, name='boot'),
 ]
 
