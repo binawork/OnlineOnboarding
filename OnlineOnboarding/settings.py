19bf520240bb838bdab0ec9ed79af7b6e78e0f78
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-07-05 16:58:32 +0200

    Fix error loading static's sources. Comment static path with None result. Move static styles according to path in index.html

--- OnlineOnboarding/settings.py
+++ OnlineOnboarding/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/onboarding/static/'
 
