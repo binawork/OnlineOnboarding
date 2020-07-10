0c1ae7cb415586543bad7ab352d4c1d8c21c96c7
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-07-10 12:22:54 +0200

    Add first temporary script for testing

---
+++ onboarding/static/js/script.js
@@ -0,0 +1,24 @@
+
+function getToken(){
+	var inputs = document.getElementsByTagName("input"), i, tok="";
+
+	for(i = inputs.length - 1; i>=0; i--){
+		if(inputs[i].name === "csrfmiddlewaretoken"){
+			tok=inputs[i].value;
+			break;
+		}
+	}
+	return tok;
+}
+
+var path = "http://localhost:8000/onboarding/api/packages/", tok, fetchProps;
+
+	tok = getToken();
+
+	fetchProps = {method:"GET", headers:{"Accept":"application/json", "X-CSRFToken":tok}};
+fetch(path, fetchProps).then(function(res){return res.json();}).then(
+	(resParsed) => {console.log(resParsed);},
+	(error) => {console.log("Can not load API, " + error);}
+);
+
+
