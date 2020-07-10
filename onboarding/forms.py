d9b063db429489a3043bcdc0cbbb57bf3b34ac86
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-07-10 12:24:50 +0200

    Add bootstrap class properties

--- onboarding/forms.py
+++ onboarding/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Page, Package
 
 
 class SignUpForm(UserCreationForm):
-    company_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    company_name = forms.CharField(max_length=30, required=False, help_text='Optional.', widget=forms.TextInput(attrs={'class':'form-control'}) )
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.', widget=forms.EmailInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
