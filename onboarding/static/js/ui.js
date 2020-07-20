1f9aa99d9ca1a32096a19a0cccd5da94f6450b74
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-07-20 10:50:42 +0200

    Add removing n-th row and prepare event listener for it (todo)

--- onboarding/static/js/ui.js
+++ onboarding/static/js/ui.js
@@ -10,8 +10,19 @@ export function clearTable(){
 	if(n > 2){
 		console.log(dTab.rows[n - 1]);
 		dTab.deleteRow(n - 1);
-		setTimeout(clearTable, 1500);
+		setTimeout(clearTable, 30);
 	}
+}
+
+export function deleteNthRow(e){
+	var aLink = ev2.target||ev2.srcElement, row = aLink.parentNode;
+
+	while(row.nodeName.toLowerCase() != "tr" && row.nodeName.toLowerCase() != "body")
+		row = row.parentNode;
+
+	if(row.nodeName.toLowerCase() == "body")
+		return;
+	row.parentNode.removeChild(row);
 }
 
 //export { clearTable as default };
