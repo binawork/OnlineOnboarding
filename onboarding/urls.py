65b8b00f7370d383b59ec321f9008413f08a3052
michalkandybowicz <eejdzent@gmail.com>
2020-07-20 11:43:07 +0200

    add API view for delete/edit package
    URL - http://127.0.0.1:8000/onboarding/api/package/int:pk/
    JSON
    {
        "id": 1,
        "title": "aaa",
        "description": "aaacvxvfffhhhh",
        "created_on": "2020-07-15T18:07:00.630728Z",
        "updated_on": "2020-07-15T18:08:04.703781Z",
        "owner": 1,
        "pages": [
            1,
            2
        ],
        "email": []
    }
    
    #24

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     # API
     path('api/packages/', views.PackageListView.as_view(), name='packages_list'),
     path('api/package/<int:pk>/', views.PackageView.as_view(), name='package_view'),
-    path('api/package/create/$/', views.CreatePackageView.as_view(), name='package_create')
+    path('api/package/create/', views.CreatePackageView.as_view(), name='package_create')
 
 ]
