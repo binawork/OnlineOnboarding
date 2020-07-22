1d87739d07d0bfd57f39374b1e857559cbc67a57
michalkandybowicz <eejdzent@gmail.com>
2020-07-22 15:59:06 +0200

    add email to package

--- onboarding/serializers.py
+++ onboarding/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from onboarding.models import Package
+from onboarding.models import Package, Email
 
 
 class PackageSerializer(serializers.ModelSerializer):
@@ -12,3 +12,18 @@ class CreatePackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Package
         fields = ['title', 'description']
+
+
+class AddEmailToPackageSerializer(serializers.ModelSerializer):
+    package = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Email
+        fields = ("email", "name", "package")
+        # extra_kwargs = {'package': {'required': True}}
+
+    # def create(self, validated_data):
+        #     package_data = validated_data.pop('package')
+        #     email = Email.objects.create(**validated_data)
+        #     Package.objects.create(email=email, **package_data)
+        #     return email
