5481184e55b6f1dd0b7b8380b383f79f388a0ca7
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-08-05 11:43:32 +0200

    Add 404 error page, change DEBUG to test this page, #22, #28

--- OnlineOnboarding/settings.py
+++ OnlineOnboarding/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-akb#of%g5eplnyyv^a1qk)^t042+8wsaavlz)gifyy$x416^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
