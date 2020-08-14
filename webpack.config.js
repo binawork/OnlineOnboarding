970f32f630553a065f2afbe413aa3cf13f2c8c3c
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-08-14 09:13:10 +0200

    Modify configurations

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'script_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // Load a custom template (lodash by default)
+      template: './src/index.html'
+    })
+  ]
+};
+
