a49ef00f1497fd9eb2e736421838acc013f908dd
michalkandybowicz <eejdzent@gmail.com>
2020-08-21 16:59:34 +0200

    import fix

--- OnlineOnboarding/settings.py
+++ OnlineOnboarding/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import onboarding.dont_touch_it_Wiktor as mail
+import OnlineOnboarding.dont_touch_it_Wiktor as mail
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
