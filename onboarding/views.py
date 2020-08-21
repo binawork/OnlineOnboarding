d6eac39b5c606101cbdf5bc1ee07801b0fe2ea73
michalkandybowicz <eejdzent@gmail.com>
2020-08-21 15:12:23 +0200

    changes in signup form

--- onboarding/views.py
+++ onboarding/views.py
@@ -6,6 +6,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+
+
 from onboarding.models import Package, ContactForm, Page, Section, User, Answer
 from .forms import SignUpForm
 from .serializers import PackageSerializer, PageSerializer, SectionSerializer, \
@@ -25,9 +36,23 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return render(request, 'index.html')
 
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse(
+                'Please confirm your email address to complete the registration'
+            )
 
     else:
         form = SignUpForm()
