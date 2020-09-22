899d20914de54057fe2a39812de882ef0dc478e5
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-09-22 12:41:06 +0200

    Import more components
    Remove unnecessary imports
    Remove obsolete, repeated meta-charset

--- onboarding/src/index.js
+++ onboarding/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+//import 'jquery/dist/jquery.min.js';
+//import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(
   <React.StrictMode>
