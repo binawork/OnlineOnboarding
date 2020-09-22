abad7da6c335aba554d296563d5eb27a863c08c4
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-09-22 12:36:22 +0200

    Add link to package; fix style of date

--- onboarding/src/Components/FormList/PackagesRow.js
+++ onboarding/src/Components/FormList/PackagesRow.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { dateToString } from "../utils";
 
 function PackagesRow(props) {
     return(
         <tr>
-            <td>{props.row.name}</td>
-            <td>{props.row.last_edit}</td>
+            <td><Link to="/package_page">{props.row.name}</Link></td>
+            <td>{ dateToString(props.row.last_edit) }</td>
             <td>edit/delete/stats</td>
         </tr>
     )
