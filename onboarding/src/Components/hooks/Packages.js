8dad8aa8b9cf754ff5a72a525a42a77f43c74710
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-09-22 12:34:15 +0200

    Add utils and hooks for AJAX requests

---
+++ onboarding/src/Components/hooks/Packages.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { getPath } from "../utils.js";
+import PackagesRow from "../FormList/PackagesRow";
+
+function Packages(){
+	var [rows , setRows] = useState([]),
+		[loaded, isLoaded] = useState(false);
+	const [error, showError] = useState(null);
+	let url = getPath(),
+		fetchProps = {method:"GET", headers:{"Accept":"application/json", "Content-Type":"application/json", "X-CSRFToken":""}};
+
+	useEffect(() => {
+		fetch(url + "api/package/", fetchProps).then(res => res.json()).then(
+			(result) => {
+				isLoaded(true);
+				setRows(result);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}, []);
+
+	if(error){
+		return <PackagesRow key={0} row={ {name: error.message, last_edit: ""} }/>
+	} else if(!loaded)
+		return <PackagesRow key={0} row={ {name: "Loading ...", last_edit: ""} }/>
+	else {
+		var form_table = [], i, count = rows.length;
+		for(i = 0; i < count; i++)
+			form_table.push(<PackagesRow key={ rows[i].id } row={ {name: rows[i].title, last_edit: rows[i].updated_on} }/>);
+		return ( <>{ form_table }</> )
+	}
+
+}
+
+export default Packages;
+
