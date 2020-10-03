7669a47b43f5520f993135e1c399b245dde34120
Anna Pakula <anna.pakula.321@gmail.com>
2020-10-03 08:04:56 +0200

    Add func to handle switcher in forms

--- onboarding/src/Components/Switcher.js
+++ onboarding/src/Components/Switcher.js
@@ -1,16 +1,21 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 //import "../static/looper/stylesheets/theme.min.css";
 //import "../static/looper/stylesheets/theme-dark.min.css";
 
-function Switcher() {
-
+function Switcher({ answRequired, handleSwitcherChange }) {
     return(
         <label className="switcher-control switcher-control-success">
-          <input type="checkbox" className="switcher-input" checked />
+          <input type="checkbox" className="switcher-input" checked={ answRequired } onChange={ handleSwitcherChange } />
           <span className="switcher-indicator"></span>
         </label>
     )
 }
-export default Switcher;
 
+Switcher.propTypes ={
+  answRequired: PropTypes.bool.isRequired,
+  handleSwitcherChange: PropTypes.func.isRequired,
+}
+
+export default Switcher;
