9b9732e582db026d64a542a39d3c45b3cba75699
Anna Pakula <anna.pakula.321@gmail.com>
2020-10-22 16:30:34 +0200

    Feat: hide card at dashboard if there are some employees #60

--- onboarding/src/Components/DashboardPage.js
+++ onboarding/src/Components/DashboardPage.js
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getPath } from "./utils.js";
 
 //import "../static/looper/stylesheets/theme.min.css";
 //import "../static/looper/stylesheets/theme-dark.min.css";
@@ -8,9 +9,37 @@ import Navbar from "./Navbar";
 import LeftMenu from "./LeftMenu";
 import PageAddressBar from "./PageAddressBar";
 
-function DashboardPage() {
+function DashboardPage(props) {
   const packageIdRef = useRef(0);
-//   console.log("dashboard: ", packageIdRef);
+
+  const [isEmployee, setIsEmployee] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [employees, setEmployees] = useState([]);
+
+  const url = getPath();
+  const fetchProps = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-CSRFToken": "",
+    },
+  };
+
+  useEffect(() => {
+    fetch(url + "api/users/", fetchProps)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          result === [] ? setIsEmployee(false) : setIsEmployee(true);
+          setIsLoaded(true);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }, [props.count]);
 
   return (
     <div className="app">
@@ -26,37 +55,49 @@ function DashboardPage() {
               <div className="page-section">
                 {" "}
                 {/* placeholder */}
-                <div className="card card-fluid p-4">
-                  <div className="card-body">
-                    Nie masz jeszcze wdrażanych pracowników (żeby wdrażać musisz
-                    mieć stworzony profil pracownika i podpięty pod niego
-                    content wdrożenia)
+                {isLoaded ? (
+                  isEmployee ? (
+                    <div className="card card-fluid p-4">
+
+                    </div>
+                  ) : (
+                    <div className="card card-fluid p-4">
+                      <div className="card-body">
+                        Nie masz jeszcze wdrażanych pracowników (żeby wdrażać
+                        musisz mieć stworzony profil pracownika i podpięty pod
+                        niego content wdrożenia)
+                      </div>
+                      <div className="card-body">
+                        <Link
+                          to={{
+                            pathname: "/add_user",
+                            state: { packageId: packageIdRef.current },
+                          }}
+                          className="menu-link"
+                        >
+                          <button className="btn btn-secondary mr-5">
+                            Stwórz profil pracownika
+                          </button>
+                        </Link>
+                        <Link
+                          to={{
+                            pathname: "/packages",
+                            state: { packageId: packageIdRef.current },
+                          }}
+                          className="menu-link"
+                        >
+                          <button className="btn btn-secondary">
+                            Stwórz content wdrożenia
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
+                  )
+                ) : (
+                  <div className="card card-fluid p-4">
+                    <div className="card-body">Loading...</div>
                   </div>
-                  <div className="card-body">
-                    <Link
-                      to={{
-                        pathname: "/add_user",
-                        state: { packageId: packageIdRef.current },
-                      }}
-                      className="menu-link"
-                    >
-                      <button className="btn btn-secondary mr-5">
-                        Stwórz profil pracownika
-                      </button>
-                    </Link>
-                    <Link
-                      to={{
-                        pathname: "/packages",
-                        state: { packageId: packageIdRef.current },
-                      }}
-                      className="menu-link"
-                    >
-                        <button className="btn btn-secondary">
-                          Stwórz content wdrożenia
-                        </button>
-                    </Link>
-                  </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
