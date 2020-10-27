576f50f0d5de14e59b827751ea4149c2b78e55f7
Anna Pakula <anna.pakula.321@gmail.com>
2020-10-27 17:36:27 +0100

    Feat: copy Q&A

--- onboarding/src/Components/QA/Qa.js
+++ onboarding/src/Components/QA/Qa.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import MarkdownArea from "../MarkdownArea";
+import { v4 as uuidv4 } from "uuid";
 
 function Qa({ id, question, answer, qaList, setQaList, provided, innerRef }) {
   const refValue = useRef(qaList);
@@ -25,6 +26,26 @@ function Qa({ id, question, answer, qaList, setQaList, provided, innerRef }) {
     });
     setQaList(answers);
   }
+  const copyQA = (e) => {
+    e.preventDefault();
+    const newId = uuidv4();
+    const newQA = {
+      id: newId,
+      question: question,
+      answer: answer
+    };
+    const index = qaList.findIndex((qa) => qa.id === id);
+    // slice() to get shallow copy of sections
+    const qaListCopy = qaList.slice(0);
+    // splice() to put copied form at the right index (next after its origin)
+    qaListCopy.splice(index + 1, 0, newQA);
+    setQaList(qaListCopy);
+  }
+  const deleteQA = (e) => {
+    e.preventDefault();
+
+    console.log(e)
+  }
   
   return (
       <div className="task-issue" {...provided.draggableProps} ref={innerRef}>
@@ -47,7 +68,7 @@ function Qa({ id, question, answer, qaList, setQaList, provided, innerRef }) {
               contentChange={changeAnswer}
             />
           </div>
-          {/* <div className="card-footer align-items-center">
+          <div className="card-footer align-items-center">
             <div className="col">
               <button className="btn" id={"copy-" + id} onClick={copyQA}>
                 <i className="fa fa-files-o fa-lg">&#61637;</i> Duplikuj
@@ -59,7 +80,7 @@ function Qa({ id, question, answer, qaList, setQaList, provided, innerRef }) {
                 Usuń
               </button>
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
   );
