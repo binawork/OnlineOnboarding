e687fcad162ff6b8a2d6d4b93706d346f9f260df
Anna Pakula <anna.pakula.321@gmail.com>
2020-11-13 15:44:41 +0100

    Add info for hr manager that q&a will show for employees automatically #82

--- onboarding/src/Components/QA/QAList.js
+++ onboarding/src/Components/QA/QAList.js
@@ -55,18 +55,34 @@ const QAList = () => {
 
   return (
     <div className="card card-fluid">
-      <div className="">
-        <div className="card-body rounded-bottom border-top">
-          {editMode ? (
-            <button className="btn btn-success" onClick={handleSave}>
-              Zapisz
-            </button>
-          ) : (
-            <button className="btn btn-success" onClick={handleEdit}>
-              Edytuj
-            </button>
-          )}
-        </div>
+      <div className="card-body rounded-bottom border-top">
+        {editMode ? (
+          <button className="btn btn-success mr-3" onClick={handleSave}>
+            Zapisz
+          </button>
+        ) : (
+          <button className="btn btn-success mr-3" onClick={handleEdit}>
+            Edytuj
+          </button>
+        )}
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          className="bi bi-info-circle mr-2"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+          />
+          <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z" />
+          <circle cx="8" cy="4.5" r="1" />
+        </svg>
+        <small>
+          <i>Zapisane Q&A pojawi się automatycznie u pracowników</i>
+        </small>
       </div>
       <div className="card-body">
         <div className="form-group">
