679d7a70e72eec31bcb4491bb7f5ab85b05ce397
Anna Pakula <anna.pakula.321@gmail.com>
2020-12-11 10:27:48 +0100

    Save name and description of package on package page after pressing 'Enter'

--- onboarding/src/Components/FormTable/FormPackageEdit.js
+++ onboarding/src/Components/FormTable/FormPackageEdit.js
@@ -28,31 +28,38 @@ function FormPackageEdit(props) {
     }
     var handleSave = (e) => {
         savePackageDetails(function(res){}, pack.packageId, pack.title, pack.description);// pack as one argument;
+        console.log('saved')
     }
+    const clickEnter = (e, buttonId) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            document.getElementById(buttonId).click();
+        }
+    };
 
     return(
         <div>
             <div className="row mb-4">
                 <div className="col">
                     <div className="has-clearable">
-                        <input type="text" value={ pack.title } onChange = { handleInputTitle } className="form-control" placeholder="Nazwa formularza" />
+                        <input type="text" value={ pack.title } onChange = { handleInputTitle } onKeyUp={ (e) => clickEnter(e, "save-form-name") } className="form-control" placeholder="Nazwa formularza" />
                     </div>
                 </div>
                 <div className="col-auto">
                     <div className="dropdown">
-                        <button className="btn btn-secondary" data-display="static" aria-expanded="false" onClick = { handleSave }>Zapisz</button>
+                        <button id="save-form-name" className="btn btn-secondary" data-display="static" aria-expanded="false" onClick = { handleSave }>Zapisz</button>
                     </div>
                 </div>
             </div>
             <div className="row mb-4">
                 <div className="col">
                     <div className="has-clearable">
-                        <input type="text" value={ pack.description } onChange = { handleInputDesc } className="form-control" placeholder="Opis Formularza" />
+                        <input type="text" value={ pack.description } onChange = { handleInputDesc } onKeyUp={ (e) => clickEnter(e, "save-form-description") } className="form-control" placeholder="Opis Formularza" />
                     </div>
                 </div>
                 <div className="col-auto">
                     <div className="dropdown">
-                        <button className="btn btn-secondary" data-display="static" aria-expanded="false" onClick = { handleSave }>Zapisz</button>
+                        <button id="save-form-description" className="btn btn-secondary" data-display="static" aria-expanded="false" onClick = { handleSave }>Zapisz</button>
                     </div>
                 </div>
             </div>
