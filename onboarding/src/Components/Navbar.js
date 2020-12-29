d82b0969201fa0f1608160511ec2143507b64f0e
Anna Pakula <anna.pakula.321@gmail.com>
2020-12-29 14:32:18 +0100

    Hide search bar on the HR's navbar #57

--- onboarding/src/Components/Navbar.js
+++ onboarding/src/Components/Navbar.js
@@ -32,16 +32,31 @@ function Navbar(props) {
             <div className="top-bar-brand pr-5" style={{height: "auto"}}>
                 <Link exact to="/" className="navbar-brand">Online Onboarding</Link>
             </div>
+
             <div className="pl-3 d-flex justify-content-center w-100">
-                <form className="form-inline w-100">
+                {/* <div className="top-bar-item px-2 d-md-none d-lg-none d-xl-none">
+                    <button 
+                        className={`hamburger hamburger-squeeze ${props.showAside ? "active" : ""}`} 
+                        type="button" 
+                        data-toggle="aside" 
+                        aria-label="toggle menu" 
+                        // onClick={() => props.setToggleAside(!props.showAside)}
+                    >
+                        <span className="hamburger-box">
+                            <span className="hamburger-inner"></span>
+                        </span>
+                    </button>
+                </div> */}
+                {/* <form className="form-inline w-100">
                     <input className="form-control col-6 mr-2" type="search" placeholder="Szukaj" aria-label="Szukaj"/>
                     <button className="btn btn-success col-auto" type="submit">Szukaj</button>
-                </form>
+                </form> */}
             </div>
-            <div className="top-bar-item px-0 d-none d-sm-flex">
+            
+            <div className="top-bar-item px-0">
                 <div className="dropdown d-flex">
                     <div className="form-inline my-2 my-lg-0">
-                        <button className="btn-account d-none d-md-flex" type="button" data-toggle="dropdown" onClick={ dropDownSwitch }>
+                        <button className="btn-account d-flex" type="button" data-toggle="dropdown" onClick={ dropDownSwitch }>
                             <span className="user-avatar user-avatar-md"><img src={ loggedUser.avatar } alt={ loggedUser.first_name + " - avatar" } /></span>
                             <span className="account-summary pr-lg-4 d-none d-lg-block">Witaj <span className="account-name">{ loggedUser.first_name }</span></span>
                         </button>
