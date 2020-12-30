d6d2dd8badb2efd1d59dda13a2df69f4c6cb2a2d
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-12-30 18:29:30 +0100

    Try experimental solution by making many-to-many between users and sections
    Table "Answers" keeps all answers for corresponding section (many-to-one)
    and "SectionsUsers" is to save users answer(s) (`data` property) for corresponding section.

--- onboarding/serializers.py
+++ onboarding/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from onboarding.models import ContactRequestDetail, Package, Page, Section, User, PackagesUsers
+from onboarding.models import ContactRequestDetail, Package, Page, Section, User, PackagesUsers, SectionsUsers
 from onboarding.models import Answer, Company, CompanyQuestionAndAnswer
 from . import mock_password
 
@@ -296,6 +296,59 @@ class AnswerSerializer(serializers.ModelSerializer):
         )
 
 
+class UserAnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SectionsUsers
+        fields = (
+            'data',
+            'updated_on'
+        )
+
+
+class SectionsUsersSerializer(serializers.ModelSerializer):
+    users = UserAnswerSerializer(source='sectionsusers_set', many=True)
+
+    class Meta:
+        ordering = ['-id']
+        model = Package
+        fields = (
+            'id',
+            'users'
+        )
+
+    """section_set = SectionSerializer(many=True)
+
+    class Meta:
+        model = SectionsUsers
+        fields = (
+            'user',
+            'section',
+            'data',
+            'updated_on',
+            'section_set',
+        )
+
+    " " "def validate(self, data):
+        " " "
+        Check that the start is before the stop.
+        " " "
+        if data['start_date'] > data['end_date']:
+            raise serializers.ValidationError("finish must occur after start")
+        return data"""
+
+
+    def create(self, validated_data):
+        pass
+        # section = validated_data.pop('section')
+        # return SectionsUsers.objects.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.data = validated_data.get('data', instance.data)
+        instance.save()
+        return instance
+
+
 class AnswersProgressStatusSerializer(serializers.ModelSerializer):
     # sen_on = serializers.ReadOnlyField(source='packageusers.send_on')
 
@@ -315,6 +368,7 @@ class WhenPackageSendToEmployeeSerializer(serializers.ModelSerializer):
         fields = (
             'send_on',
         )
+
 
 
 # SECTION with ANSWERS
