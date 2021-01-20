61339fadd06acd418034df380f869a8bd843a28a
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-01-20 15:35:06 +0100

    Add basic view of component; #164
    Add documentation about component

--- onboarding/src/Components/EmployeeAnswersView/EmployeeAnswersViewPage.js
+++ onboarding/src/Components/EmployeeAnswersView/EmployeeAnswersViewPage.js
@@ -1,7 +1,17 @@
 import React, { useRef } from "react";
+import LoggedUser from "../hooks/LoggedUser.js";
+import Navbar from "../Navbar";
+import LeftMenu from "../LeftMenu";
 
 
+/**
+ * Loads page with answers for a page answered by employee
+ * @param props - location.state: {packageId: number, loggedUser: Object, pageId: number, employee: Object}
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function EmployeeAnswersViewPage(props){
+    document.title = "Onboarding: odpowiedzi pracownika";
     const packageIdRef = useRef(0);
     let loggedUser;
     if(props.location.state){
@@ -12,7 +22,16 @@ function EmployeeAnswersViewPage(props){
 
 
     return(
-        <></>
+        <div className="app">
+            <header className="app-header app-header-dark">
+                <Navbar loggedUser={ loggedUser } />
+            </header>
+            <LeftMenu packageId = { packageIdRef.current } loggedUser={ loggedUser } />
+            <main className="app-main">
+                <div className="wrapper">
+                </div>
+            </main>
+        </div>
     )
 }
 
