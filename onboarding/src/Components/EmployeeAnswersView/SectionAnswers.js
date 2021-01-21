9227eae05d99691bbb47fc5e729560ebd39ef42b
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-01-21 14:09:59 +0100

    Create components to present employees answers; #164
    SectionAnswers has to be finished

---
+++ onboarding/src/Components/EmployeeAnswersView/SectionAnswers.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+/**
+ * 
+ * @param props - {answerData: Object}
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function SectionAnswers(props) {
+    // todo: list of options and answers;
+
+    return(
+        <></>
+    )
+}
+
+export default SectionAnswers;
+
