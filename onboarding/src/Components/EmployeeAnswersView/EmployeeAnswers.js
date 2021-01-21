304c218b685605116faa801546dc8c4f621ad2dd
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-01-21 09:41:53 +0100

    Develop requesting and receiving employees answers; #164

--- onboarding/src/Components/EmployeeAnswersView/EmployeeAnswers.js
+++ onboarding/src/Components/EmployeeAnswersView/EmployeeAnswers.js
@@ -1,20 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getEmployeesSectionsAndAnswers } from "../hooks/EmployeeForms";
 //import PropTypes from "prop-types";
 
 
 function EmployeeAnswers(props){
-  const [loadingMessage, setMessage] = useState({message: "Ładowanie...", print: true});
+    const [sectionsAnswers, setSectionsAnswers] = useState({sections: [], answers: [], answers_cp: []});
+    const [loadingMessage, setMessage] = useState({message: "Ładowanie...", print: true});
+    const [sectionsView, setView] = useState([]);
 
 
-  return (
+    const setErrorMessage = function(message){
+        setMessage({message: message, print: true});
+    };
+
+    const showSectionsAnswers = (sectionsAnswersResult, employeeDidAnswer) => {
+        console.log(employeeDidAnswer);
+        console.log(sectionsAnswersResult);
+        //let newSectionsView = [];
+        setMessage({message: "Pobrano", print: true});
+        //setSectionsAnswers(sectionsAnswersResult);
+    };
+
+
+    useEffect(() => {
+        if(typeof props.employeeId === 'undefined' || props.employeeId === null ||
+            props.employeeId < 1 || props.employeeId === false){
+            setMessage({message: "Nie zdefiniowano pracownika!", print: true});
+        } else {
+            getEmployeesSectionsAndAnswers(props.pageId, props.employeeId, setErrorMessage, showSectionsAnswers);
+            //return () => clearInterval(saveInterval);
+        }
+    }, [props.pageId]);
+
+
+    return (
       <div className="card card-fluid">
         { loadingMessage.print ? (
             <div className="p-3">{ loadingMessage.message }</div>
         ) : (
-            <></>
+            sectionsView
         ) }
       </div>
-  );
+    );
 }
 
 /*EmployeeAnswers.propTypes = {
