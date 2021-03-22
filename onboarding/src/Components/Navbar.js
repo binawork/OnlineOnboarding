5fe2ff63667c2725f213465578ecc73b6fb25da5
Anna Pakula <anna.pakula.321@gmail.com>
2021-03-22 11:45:08 +0100

    Change position and add icons for navbar's dropdown list #155

--- onboarding/src/Components/Navbar.js
+++ onboarding/src/Components/Navbar.js
@@ -31,43 +31,42 @@ function Navbar({ loggedUser }) {
             
             <div className="top-bar-item px-0">
                 <div className="dropdown d-flex">
-                    <div className="form-inline my-2 my-lg-0">
-                        <button 
-                            className="btn-account d-flex" 
+                    <button
+                        className="btn-account d-flex"
+                        onClick={ dropDownSwitch }
+                    >
+                        <span className="user-avatar user-avatar-md">
+                            <img src={ loggedUser.avatar } alt={ loggedUser.first_name + " - avatar" } />
+                        </span>
+                        <span className="account-summary pr-lg-4 d-none d-lg-block">
+                            Witaj
+                            <span className="account-name">
+                                { loggedUser.first_name }
+                            </span>
+                        </span>
+                    </button>
+
+                    <div className="dropdown-menu" id="userDropDown" style={{ display: dropDownList, position: "absolute", top: "56px", left: "-84px" }}>
+                        <div className="dropdown-arrow dropdown-arrow-right"></div>
+                        {/*<h6 className="dropdown-header d-none d-md-block d-lg-none">  user username</h6>
+                        <a className="dropdown-item" href="#"><span className="dropdown-icon oi oi-person"></span> Profil</a> */}
+                        <Link
+                            to="/profile/manager"
+                            className="dropdown-item"
+                            data-toggle="tooltip"
                             onClick={ dropDownSwitch }
                         >
-                            <span className="user-avatar user-avatar-md">
-                                <img src={ loggedUser.avatar } alt={ loggedUser.first_name + " - avatar" } />
-                            </span>
-                            <span className="account-summary pr-lg-4 d-none d-lg-block">
-                                Witaj 
-                                <span className="account-name">
-                                    { loggedUser.first_name }
-                                </span>
-                            </span>
-                        </button>
-
-                        <div className="dropdown-menu" id="userDropDown" style={{ display: dropDownList }}>
-                            <div className="dropdown-arrow ml-3"></div>
-                            {/*<h6 className="dropdown-header d-none d-md-block d-lg-none">  user username</h6>
-                            <a className="dropdown-item" href="#"><span className="dropdown-icon oi oi-person"></span> Profil</a> */}
-                            <Link 
-                                to="/profile/manager"
-                                className="dropdown-item" 
-                                data-toggle="tooltip"
-                                onClick={ dropDownSwitch }
-                            >
-                                <span className="dropdown-icon oi oi-person"></span>
-                                Profil
-                            </Link>
-                            <a
-                                className="dropdown-item"
-                                href={"/accounts/logout/"}
-                                onClick={ handleLogout }
-                            >
-                                <span className="dropdown-icon oi oi-account-logout"></span> Wyloguj
-                            </a>
-                        </div>
+                            <i className="bi bi-person mr-2" style={{ fontSize: "18px"}}></i>
+                            Profil
+                        </Link>
+                        <a
+                            className="dropdown-item"
+                            href={"/accounts/logout/"}
+                            onClick={ handleLogout }
+                        >
+                            <i className="bi bi-box-arrow-left mr-2" style={{ fontSize: "18px"}}></i>
+                            Wyloguj
+                        </a>
                     </div>
                 </div>
             </div>
