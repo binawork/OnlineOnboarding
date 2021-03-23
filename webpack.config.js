10128d4bfef70494f8a4d4eeeba1b8ec8284d9f2
Anna Pakula <anna.pakula.321@gmail.com>
2021-03-23 15:10:39 +0100

    Add sass

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,20 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
         test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
         use: {
           loader: 'url-loader',
