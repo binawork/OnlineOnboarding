2dbeb19357d2b130453050b47826e765bcbfd6ee
Anna Pakula <anna.pakula.321@gmail.com>
2021-03-30 13:12:03 +0200

    Change buttons' text to icons on packages page with smaller width #155

--- onboarding/src/Components/PackagesList/PackagesListTable.js
+++ onboarding/src/Components/PackagesList/PackagesListTable.js
@@ -91,7 +91,7 @@ function PackagesListTable({ setPackagesList }) {
             <button className="btn btn-success mb-3" onClick={() => setShowAddCatalogueBox(true)}>+ Dodaj katalog</button>
             { showAddCatalogueInput &&
                 <div className="modal modal-alert fade show d-flex flex-column justify-content-center" style={{ backdropFilter: "brightness(55%)"}} onClick={() => setShowAddCatalogueBox(false)}>
-                    <div className="modal-dialog w-100"  onClick={ e => e.stopPropagation() }>
+                    <div className="modal-dialog"  onClick={ e => e.stopPropagation() }>
                         <div className="modal-content">
                             <i className="bi bi-x-circle close-icon" onClick={() => setShowAddCatalogueBox(false)} style={{cursor: "pointer"}} />
                             <div className="card-header">
