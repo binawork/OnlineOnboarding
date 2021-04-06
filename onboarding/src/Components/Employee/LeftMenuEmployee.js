c59aeaa9ab83db20e03ec4e0983f233b2071461d
Anna Pakula <anna.pakula.321@gmail.com>
2021-04-06 14:48:27 +0200

    Make responsive employee left menu #155

--- onboarding/src/Components/Employee/LeftMenuEmployee.js
+++ onboarding/src/Components/Employee/LeftMenuEmployee.js
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
-
+import React from "react";
+import ModeButton from "../ModeButton";
+import "../../static/css/LeftMenu.scss";
 
 function LeftMenuEmployee(props) {
 
@@ -29,40 +30,63 @@ function LeftMenuEmployee(props) {
 
 
   return (
-    <aside className={`app-aside app-aside-expand-md app-aside-light ${props.showAside ? "show" : ""}`} style={{zIndex: "9"}}>
-      <div className="aside-content mx-3 my-5 mx-sm-5">
-        <header className="aside-header d-block d-md-none"></header>
-        <div className="aside-menu overflow-hidden">
-          <nav id="stacked-menu" className="stacked-menu">
-            <ul className="menu">
-              <li className="menu-item">
-                <a href="#" onClick={ goToDashboard } className="menu-link">
-                  <i className="bi bi-diagram-2 mr-2" style={{ fontSize: "18px"}}></i>
-                  <span className="menu-text">Wdrożenia</span>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="#" onClick={ goToUserPage } className="menu-link">
-                  <i className="bi bi-person mr-2" style={{ fontSize: "18px"}}></i>
-                  <span className="menu-text">Mój profil</span>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="#" onClick={ goToQnA } className="menu-link">
-                  <i className="bi bi-question-circle mr-2" style={{ fontSize: "18px"}}></i>
-                  <span className="menu-text">Q&A</span>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="#" onClick={ goToCompanyInfoPage } className="menu-link">
-                  <i className="bi bi-building mr-2" style={{ fontSize: "18px"}}></i>
-                  <span className="menu-text">O firmie</span>
-                </a>
-              </li>
-              <li className="menu-item">
-              </li>
-            </ul>
-          </nav>
+    <aside
+      className={`LeftMenu app-aside app-aside-expand-md
+      app-aside-light ${props.showAside ? "show" : ""}`}
+      style={{zIndex: "9"}}
+    >
+      <div
+        className={`${props.showAside ? "LeftMenu__pseudo-wrapper" : ""} w-100 h-100`}
+        onClick={ () => props.setToggleAside(false) }
+      >
+        <div className="LeftMenu__wrapper">
+          <div className="LeftMenu__overflow aside-menu overflow-hidden">
+            <nav id="stacked-menu" className="stacked-menu">
+              <ul className="LeftMenu__list">
+                <li className="menu-item has-child">
+                  <a
+                    href="#"
+                    onClick={ goToDashboard }
+                    className="LeftMenu__link menu-link"
+                  >
+                    <i className="bi bi-diagram-2 mr-2" style={{ fontSize: "18px"}}></i>
+                    <span className="menu-text">Wdrożenia</span>
+                  </a>
+                </li>
+                <li className="menu-item">
+                  <a
+                    href="#"
+                    onClick={ goToUserPage }
+                    className="LeftMenu__link menu-link"
+                  >
+                    <i className="bi bi-person mr-2" style={{ fontSize: "18px"}}></i>
+                    <span className="menu-text">Mój profil</span>
+                  </a>
+                </li>
+                <li className="menu-item">
+                  <a
+                    href="#"
+                    onClick={ goToQnA }
+                    className="LeftMenu__link menu-link"
+                  >
+                    <i className="bi bi-question-circle mr-2" style={{ fontSize: "18px"}}></i>
+                    <span className="menu-text">Q&A</span>
+                  </a>
+                </li>
+                <li className="menu-item">
+                  <a
+                    href="#"
+                    onClick={ goToCompanyInfoPage }
+                    className="LeftMenu__link menu-link"
+                  >
+                    <i className="bi bi-building mr-2" style={{ fontSize: "18px"}}></i>
+                    <span className="menu-text">O firmie</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <ModeButton />
         </div>
       </div>
     </aside>
