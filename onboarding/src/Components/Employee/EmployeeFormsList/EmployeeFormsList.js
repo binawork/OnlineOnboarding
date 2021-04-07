956fa7268a6a33bcca2fb1ccbc726f5593edaa62
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-04-07 15:09:24 +0200

    Add state for error case

--- onboarding/src/Components/Employee/EmployeeFormsList/EmployeeFormsList.js
+++ onboarding/src/Components/Employee/EmployeeFormsList/EmployeeFormsList.js
@@ -1,14 +1,15 @@
-import React/*, { useState, useEffect }*/ from "react";
+import React, { useState } from "react";
 import EmployeeFormsTable from "./EmployeeFormsTable";
 import LoggedUser from "../../hooks/LoggedUser";
 import { SingleEmployeeForms } from "../../hooks/EmployeeForms";
 
 
 function EmployeeFormsList(props) {
+    const [isError, setError] = useState(false);
     /*const [loggedUser, setLoggedUser] = useState({id: 0, email: "", first_name: "", last_name: "",
     							phone_number: "", location: "", team: "",
     							job_position: "",last_login: "", avatar: ""});*/
-    const employeeForms = SingleEmployeeForms(0, function(){}, function(){});
+    const employeeForms = SingleEmployeeForms(0, setError, function(){});
     let loggedUser;
     if(props.loggedUser){
         loggedUser = props.loggedUser;
