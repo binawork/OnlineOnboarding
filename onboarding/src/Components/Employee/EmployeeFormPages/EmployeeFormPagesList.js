6b379ef4e840d9590af7efb503abc487eac1acab
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-04-09 14:18:38 +0200

    Add progress column into the list of pages

--- onboarding/src/Components/Employee/EmployeeFormPages/EmployeeFormPagesList.js
+++ onboarding/src/Components/Employee/EmployeeFormPages/EmployeeFormPagesList.js
@@ -1,20 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ProgressBar from "../../ProgressBar";
 
-const EmployeeFormPagesList = ({ pagesList, switchPage }) => {
+
+const EmployeeFormPagesList = ({ pagesList, switchPage, progress }) => {
   const moveToForm = (page) => {
-    switchPage(page);
-  };
+    let pageProgress = {}, pageId = parseInt(page.id, 10);
+    if(progress.hasOwnProperty("pages") && progress.pages.hasOwnProperty(pageId) ){
+      pageProgress = progress.pages[pageId];
+    }
 
-  const pages = pagesList.map((page) => (
-    <tr key={page.id}>
-      <td>
-        <a href="#" onClick={(e) => moveToForm(page)}>
-          {page.title}
-        </a>
-      </td>
-    </tr>
-  ));
+
+    switchPage(page, pageProgress);
+  };
+  const notStartedMsg = "Nie rozpoczęte", inProgressMsg = "W trakcie", finishedMsg = "Skończone";
+
+  const pages = pagesList.map((page) => {
+    let progressForPage = <ProgressBar backgroundSize={ "0%" } />, progressMsg = "", pageId = parseInt(page.id, 10);
+
+    if(progress.hasOwnProperty("pages") ){
+      progressForPage = <ProgressBar backgroundSize={ "0%" } />
+      progressMsg = <small className="ml-1">{ notStartedMsg }</small>
+
+      let page, percentage;//, finishMsg = notStartedMsg;
+      if(progress.pages.hasOwnProperty(pageId) ){
+        page = progress.pages[pageId];
+
+        if(page.finished){
+          percentage = "100%";
+          // finishMsg = finishedMsg;
+          progressForPage = <ProgressBar color={ "green" } backgroundSize={ percentage } />
+          progressMsg = <small className="ml-1">{ finishedMsg }</small>
+        } else {
+          percentage = "50%";
+          // finishMsg = inProgressMsg;
+          progressForPage = <ProgressBar color={ "yellow" } backgroundSize={ percentage } />
+          progressMsg = <small className="ml-1">{ inProgressMsg }</small>
+        }
+
+      }
+
+    }
+
+    return (
+      <tr key={page.id}>
+        <td>
+          <a href="#" onClick={(e) => moveToForm(page)}>
+            {page.title}
+          </a>
+        </td>
+        <td className="form-progress">
+          { progressForPage }{ progressMsg }
+        </td>
+      </tr>
+    )
+  });
+
 
   return <>{pages}</>;
 };
