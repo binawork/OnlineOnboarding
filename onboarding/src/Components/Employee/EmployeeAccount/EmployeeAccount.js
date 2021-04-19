797935179d796202a580a476f2a74095fd0cb217
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-04-19 18:27:19 +0200

    Use Promise-based avatar uploading function with function updating data of the user; #224

--- onboarding/src/Components/Employee/EmployeeAccount/EmployeeAccount.js
+++ onboarding/src/Components/Employee/EmployeeAccount/EmployeeAccount.js
@@ -4,10 +4,10 @@ import EmployeeAvatar from "./EmployeeAvatar";
 import EmployeeProfile from "./EmployeeProfile";
 import ModalWarning from "../../ModalWarning";
 import PageAddressBar from "../../PageAddressBar";
-import { uploadAvatar, employeeSelfEdit } from "../../hooks/Users";
+import { uploadAvatarSync, employeeSelfEdit } from "../../hooks/Users";
 
 
-function EmployeeAccount({ loggedUser }){
+function EmployeeAccount({ loggedUser, countUpdate, updateUser }){
     const [employeeModal, setModal ] = useState(<></>);
     let editedUser = {...loggedUser};
 
@@ -22,26 +22,31 @@ function EmployeeAccount({ loggedUser }){
     if(typeof editedUser.avatar !== "string" || editedUser.avatar.length < 1)
         editedUser.avatar = "/onboarding/static/images/unknown-profile.jpg";
 
-    const [imageFile, setImageFile] = useState({localChanged: false, img: editedUser.avatar});
+    const [imageFile, setImageFile] = useState({localChanged: false, img: editedUser.avatar, msg: ""});
 
     const handleEdit = function(user){
-        if(imageFile.localChanged){
-            console.log("EAc - av");
-            uploadAvatar(updateImage, imageFile.img, user);
-        }
+        uploadAvatarSync(imageFile, user).then(function(result){
+            if(typeof result.avatarData === "string"){
+                setImageFile({localChanged: false, img: result.avatarData, msg: result.avatarMsg?result.avatarMsg:""});
+            }
 
-        user.name = user.first_name;
-        employeeSelfEdit(showModal, user);
+            return result;
+        }).then((result) => {
+            let user = {...result};
+            if(user.avatarData)
+                delete user.avatarData;
+            employeeSelfEdit(showModal, user);
+        });
     };
 
     const changeImage = (file) => {
-        setImageFile({localChanged: true, img: file});
+        setImageFile({localChanged: true, img: file, msg: ""});
     };
 
-    const updateImage = function(response){
+    /*const updateImage = function(response){
         if(typeof response.avatar === "string")
-            setImageFile({localChanged: false, img: response.avatar});
-    };
+            setImageFile({localChanged: false, img: response.avatar, msg: ""});
+    };*/
 
     const showModal = (message) => {
         setModal(
@@ -56,6 +61,7 @@ function EmployeeAccount({ loggedUser }){
     };
     const hideModal = function(){
         setModal(<></>);
+        updateUser(countUpdate + 1);
     };
 
     return (
