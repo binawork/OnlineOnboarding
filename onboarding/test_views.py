7c33aac6a8e1021bb1b8b7d3d5bd810c5f75086c
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-05-21 17:19:40 +0200

    Add test class for package

--- onboarding/test_views.py
+++ onboarding/test_views.py
@@ -3,6 +3,8 @@ from unittest import TestCase
 from django.test import Client, TestCase
 import os
 
+from onboarding.models import User
+
 
 class RegisterTests(TestCase):
 
@@ -55,3 +57,46 @@ class TestAddUserToPackageViewSet(TestCase):
 
         self.assertEqual(b'Please confirm your email address to complete the registration', response.content)
         self.assertEqual(200, response.status_code)
+
+
+class PackageViewTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineOnboarding.settings')
+
+        user = User.objects.create(username='john@example.com');
+        user.set_password('secret')
+        user.save()
+
+        cls.user = user
+
+        cls.client = Client()
+        # cls.client.login(username='john@example.com', password='secret')
+
+    """@classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='john@example.com');
+        user.set_password('secret')
+        user.save()
+
+        cls.user = user"""
+
+    """@classmethod
+    def tearDownClass(cls):
+        User.objects.all().delete()
+        super().tearDownClass()"""
+
+    def test_access_get(self):
+        logged_user = self.client.login(username=self.user.username, password='secret')
+        self.assertTrue(logged_user)
+
+        response = self.client.get('/api/package/')
+        self.assertEqual(200, response.status_code)
+
+
+# knowledge base
+
+# https://docs.djangoproject.com/en/3.2/topics/testing/tools/;
+# run to keep database from local_settings: python manage.py test --keepdb
+
