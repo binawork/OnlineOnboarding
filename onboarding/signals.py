eef96e6d320ce5e33daa19999975f1f26f2f12a2
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-06-30 17:46:50 +0200

    Add through model to list users with their sessions; #239
    Add new elements after every loggin in

--- onboarding/signals.py
+++ onboarding/signals.py
@@ -1,12 +1,15 @@
+from django.contrib.sessions.models import Session
 from django.contrib.auth import user_logged_in, user_logged_out
 from django.dispatch import receiver
+from .models import UserSessions
 
 
 @receiver(user_logged_in)
 def user_logged_in(sender, request, user, **kwargs):
-	pass
+    session = Session.objects.get(pk=request.session.session_key)
+    UserSessions.objects.get_or_create(user=user, session=session)
 
 
 @receiver(user_logged_out)
 def user_logged_out(sender, request, user, **kwargs):
-	pass
+    pass
