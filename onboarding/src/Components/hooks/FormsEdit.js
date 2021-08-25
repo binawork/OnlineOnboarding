dfcc34466012fd681eb43be93b9f1dc7c7c5d428
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2021-08-25 18:02:55 +0200

    Add functions to request a list of files and to delete specific file; #160

--- onboarding/src/Components/hooks/FormsEdit.js
+++ onboarding/src/Components/hooks/FormsEdit.js
@@ -102,5 +102,63 @@ export function savePageDetails(handleSuccess, pageId, title, link, description)
 	return true;
 }
 
+/**
+ * Requests files for page and works on them by callback function
+ *
+ * @param pageId - an id of page for which we want to reqests and get files;
+ * @param setterForFilesState - callback function to call when success;
+ * @param showErrorCallback - callback function to show an error of request;
+ * @returns {*} - abort object fro Promise-fetch;
+ */
+export function getFilesForPage(pageId, setterForFilesState, showErrorCallback){
+	let fetchProps = {method:"GET", headers:{"Accept":"application/json", "Content-Type":"application/json", "X-CSRFToken":""}},
+		abortCont = new AbortController();
+	var url = getPath();
+	fetchProps.signal = abortCont.signal;
+
+	fetch(url + "api/page/" + pageId + "/files/", fetchProps)
+		.then(res => {
+			if(!res.ok)
+				throw Error("Wystąpił błąd podczas pobierania listy plików!");
+			return res.json();
+		})
+		.then( (response) => {
+			let files = [], row, downloadUrl = url + "api/page_file/";// 1/download/";
+        	response.forEach(function(element){
+        		row = element;
+        		row.data_file = downloadUrl + element.id + "/download/";
+        		files.push(row);
+        	});
+        	setterForFilesState(files);
+        })
+		.catch(error => {
+			showErrorCallback(error.message);
+		});
+
+	return abortCont;
+}
+
+/**
+ * Removes file of pages in packages.
+ * @param pageFileId - id of file to be removed;
+ * @param removeCallback - callback function to be called when request is finished;
+ * @returns {*};
+ */
+export function removePageFile(pageFileId, removeCallback){
+	let url = getPath(), token = getCookie('csrftoken'),
+		fetchProps = {method:"DELETE", headers:{"Accept":"application/json", "Content-Type":"application/json", "X-CSRFToken":token}};
+
+	fetch(url + "api/page_file/" + pageFileId + "/", fetchProps)
+		.then(res => {
+			if(!res.ok)
+				throw Error("Błąd: nie udało się usunąć pliku!");
+			return (res.status !== 204) ? res.json() : res;// 204: HTTP_204_NO_CONTENT;
+		}).then(data => {
+			removeCallback("Udało się usunąć plik.", pageFileId);
+		}).catch(err => {
+			removeCallback(err.message);
+		});
+}
+
 export default FormsEdit;
 
